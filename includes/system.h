9f19250ec6e9f0b3a35e455808d137de1dc73e11
stdmedoth <jovictor210@gmail.com>
2023-06-14 23:34:05 -0300

    added internal forces

--- includes/system.h
+++ includes/system.h
@@ -2,11 +2,13 @@
 #define __MATRIX_SYSTEM__
 
 #include <punctual_body.h>
+#include <force.h>
 
 typedef struct  _system
 {
     PunctualBody *mcs; // mass centers
     PunctualBody *cm;  // mass center
+    Force *forces; // internal forces
 } System;
 
 #endif
